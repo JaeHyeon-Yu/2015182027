6e880d79fdcff8891892eacdbe7d70a970c99c1a
JaeHyeon-Yu <43131666+JaeHyeon-Yu@users.noreply.github.com>
2018-09-17 17:34:55 +0900

    1번 좌표로 이동!
    X, Y의 양의 축!

--- Drill05/character_moves.py
+++ Drill05/character_moves.py
@@ -5,10 +5,35 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
+x, y = 0+25, 90
+frame_x, frame_y = 0, 100
+move = 5
+
+def Move_Right(x, frame_x, xpos):
+    while (x < xpos):
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame_x * 100, frame_y, 100, 100, x, y)
+        update_canvas()
+        frame_x = (frame_x + 1) % 8
+        x = x + move
+        delay(0.01)
+        get_events()
+def Move_Up(y, frame_x,xpos, ypos):
+    x=xpos
+    while (y < ypos):
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame_x * 100, frame_y, 100, 100, x, y)
+        update_canvas()
+        frame_x = (frame_x + 1) % 8
+        y = y + move
+        delay(0.01)
+        get_events()
 
 def Move_to_Position(xpos, ypos):
-    pass
-
+       Move_Right(x, frame_x, xpos)
+       Move_Up(y, frame_x,xpos, ypos)
 
 while True:
     Move_to_Position(203, 535)
