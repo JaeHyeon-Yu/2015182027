ece64fb4aba58e7bc7255126874761003a2b4bf5
JaeHyeon-Yu <43131666+JaeHyeon-Yu@users.noreply.github.com>
2018-09-17 21:39:21 +0900

    Drill05 좌측대각선도 구현!
    좌측 대각선 방향으로 이동하는 것을 구현했다.
    Commit 05

--- Drill05/character_moves.py
+++ Drill05/character_moves.py
@@ -6,34 +6,47 @@ grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
 x, y=0+25, 90
-frame_x, frame_y = 0, 100
+#x, y = 503, 535
+frame_x = 0
 
 def Move_Right(xpos, ypos):
-    global x, y, frame_x, frame_y
+    global x, y, frame_x
     slope = (ypos-y)/(xpos-x)
     xp, yp = x, y
     while (x <= xpos):
         clear_canvas()
         grass.draw(400, 30)
-        character.clip_draw(frame_x * 100, frame_y, 100, 100, x, y)
+        character.clip_draw(frame_x * 100, 100, 100, 100, x, y)
         update_canvas()
         frame_x = (frame_x + 1) % 8
-    #    y = slope * (x-xp) + yp
-        delay(0.05)
+        delay(0.02)
         get_events()
         y = slope * (x-xp) + yp
         x+=1
 
 
 def Move_Left(xpos, ypos):
-    pass
+    global x, y, frame_x
+    slope = (ypos - y) / (xpos - x)
+    xp, yp = x, y
+    while (x >= xpos):
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame_x * 100, 200, 100, 100, x, y)
+        update_canvas()
+        frame_x = (frame_x + 1) % 8
+        delay(0.02)
+        get_events()
+        y = slope * (x - xp) + yp
+        x -= 1
 
 
 def Move_to_Position(xpos, ypos):
-     if (x < xpos):
-         Move_Right(xpos, ypos)
-     else :
-         Move_Left(xpos, ypos)
+    global x, y
+    if (x < xpos):
+        Move_Right(xpos, ypos)
+    elif (x > xpos) :
+        Move_Left(xpos, ypos)
 
 while True:
     Move_to_Position(203, 535)
