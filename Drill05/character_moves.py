5236e9375c0da9b1b7a96b42954a83d927c4c390
JaeHyeon-Yu <43131666+JaeHyeon-Yu@users.noreply.github.com>
2018-09-17 16:56:23 +0900

    Drill05 뼈대완성
    Drill05 뼈대완성
    
    Commit 01

--- Drill05/character_moves.py
+++ Drill05/character_moves.py
@@ -3,79 +3,24 @@ from pico2d import *
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('character.png')
+character = load_image('animation_sheet.png')
 
-def move_from_center_to_right():
-    x, y = 800 // 2, 90
-    while x < 800 -25:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        x += 2
-        delay(0.01)
 
-def move_up():
-    x, y = 800 - 25, 90
-    while y < 600 - 50:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        y += 2
-        delay(0.01)
-
-def move_left():
-    x, y = 800 - 25, 600-50
-    while x > 0 + 25:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        x -= 2
-        delay(0.01)
-
-def move_down():
-    x, y = 0 + 25, 600 - 50
-    while y > 90:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        y -= 2
-        delay(0.01)
-
-def move_from_left_to_center():
-    x, y = 0 + 25, 90
-    while x < 400:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        x += 2
-        delay(0.01)
-
-def make_rectangle():
-    move_from_center_to_right()
-    move_up()
-    move_left()
-    move_down()
-    move_from_left_to_center()
+def Move_to_Position(xpos, ypos):
     pass
 
-import math
-def make_circle():
-    cx, cy, r = 800 // 2, 600 // 2, (600-100)//2
-    degree = -90
-
-    while degree < 270:
-        radian = math.radians(degree)
-        x = cx + r*math.cos(radian)
-        y = cy + r*math.sin(radian)
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        degree += 1
-        delay(0.01)
 
 while True:
-    make_rectangle()
-    make_circle()
+    Move_to_Position(203, 535)
+    Move_to_Position(132, 243)
+    Move_to_Position(535, 470)
+    Move_to_Position(477, 203)
+    Move_to_Position(715, 136)
+    Move_to_Position(316, 225)
+    Move_to_Position(510, 92)
+    Move_to_Position(692, 518)
+    Move_to_Position(682, 336)
+    Move_to_Position(712, 349)
 
 
 close_canvas()
