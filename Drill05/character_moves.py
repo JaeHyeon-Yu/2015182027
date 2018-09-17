bcff26954fd11aa0489bdd36e22e47017757073d
JaeHyeon-Yu <43131666+JaeHyeon-Yu@users.noreply.github.com>
2018-09-17 16:03:51 +0900

    프로그램 전체 뼈대 완성
    프로그램 전체 뼈대 완성

--- Drill05/character_moves.py
+++ Drill05/character_moves.py
@@ -5,6 +5,16 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-# fill here
+def make_rectangle():
+    pass
+
+def make_circle():
+    pass
+
+while True:
+    make_rectangle()
+    make_cirle()
+
+#함수 이름의 시작은 항상 동사
 
 close_canvas()
