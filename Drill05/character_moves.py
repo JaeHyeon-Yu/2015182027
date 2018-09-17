e821779cef9aedff6da8d594aa4edc96fda9bb38
JaeHyeon-Yu <43131666+JaeHyeon-Yu@users.noreply.github.com>
2018-09-17 21:43:37 +0900

    이동 애니메이션 함수로 뺌.
    좌, 우측 대각선 각각에 들어있던 애니메이션을 함수로 빼버림.
    
    Commit 06

--- Drill05/character_moves.py
+++ Drill05/character_moves.py
@@ -6,40 +6,35 @@ grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
 x, y=0+25, 90
-#x, y = 503, 535
 frame_x = 0
+
+def Move_Animation(frame_y) :
+    global x, y, frame_x
+    clear_canvas()
+    grass.draw(400, 30)
+    character.clip_draw(frame_x * 100, frame_y, 100, 100, x, y)
+    update_canvas()
+    frame_x = (frame_x + 1) % 8
+    delay(0.02)
+    get_events()
 
 def Move_Right(xpos, ypos):
     global x, y, frame_x
     slope = (ypos-y)/(xpos-x)
     xp, yp = x, y
     while (x <= xpos):
-        clear_canvas()
-        grass.draw(400, 30)
-        character.clip_draw(frame_x * 100, 100, 100, 100, x, y)
-        update_canvas()
-        frame_x = (frame_x + 1) % 8
-        delay(0.02)
-        get_events()
+        Move_Animation(100)
         y = slope * (x-xp) + yp
         x+=1
-
 
 def Move_Left(xpos, ypos):
     global x, y, frame_x
     slope = (ypos - y) / (xpos - x)
     xp, yp = x, y
     while (x >= xpos):
-        clear_canvas()
-        grass.draw(400, 30)
-        character.clip_draw(frame_x * 100, 200, 100, 100, x, y)
-        update_canvas()
-        frame_x = (frame_x + 1) % 8
-        delay(0.02)
-        get_events()
+        Move_Animation(200)
         y = slope * (x - xp) + yp
         x -= 1
-
 
 def Move_to_Position(xpos, ypos):
     global x, y
