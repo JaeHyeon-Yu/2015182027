f23c191325550e75807a5ecde2ba72efc060bb75
JaeHyeon-Yu <43131666+JaeHyeon-Yu@users.noreply.github.com>
2018-09-17 16:08:43 +0900

    사각형 그리기의 함수추가함
    사각형 그리기의 함수추가함
    
    **함수내용 미완.

--- Drill05/character_moves.py
+++ Drill05/character_moves.py
@@ -5,7 +5,23 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+def move_from_center_to_right():
+    pass
+def move_up():
+    pass
+def move_left():
+    pass
+def move_down():
+    pass
+def move_from_left_to_center():
+    pass
+
 def make_rectangle():
+    move_from_center_to_right()
+    move_up()
+    move_left()
+    move_down()
+    move_from_left_to_center()
     pass
 
 def make_circle():
@@ -15,6 +31,5 @@ while True:
     make_rectangle()
     make_cirle()
 
-#함수 이름의 시작은 항상 동사
 
 close_canvas()
