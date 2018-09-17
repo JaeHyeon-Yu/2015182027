9f251392e9ccc7aeaa26d69d3f30d56310285521
JaeHyeon-Yu <43131666+JaeHyeon-Yu@users.noreply.github.com>
2018-09-17 21:12:12 +0900

    Drill05 대각선 우측이동
    우측 대각선 방향으로 이동하는 것을 구현했다.
    Commit 04

--- Drill05/character_moves.py
+++ Drill05/character_moves.py
@@ -6,19 +6,34 @@ grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
 x, y=0+25, 90
+frame_x, frame_y = 0, 100
 
-def Move_Right():
-    pass
+def Move_Right(xpos, ypos):
+    global x, y, frame_x, frame_y
+    slope = (ypos-y)/(xpos-x)
+    xp, yp = x, y
+    while (x <= xpos):
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame_x * 100, frame_y, 100, 100, x, y)
+        update_canvas()
+        frame_x = (frame_x + 1) % 8
+    #    y = slope * (x-xp) + yp
+        delay(0.05)
+        get_events()
+        y = slope * (x-xp) + yp
+        x+=1
 
-def Move_Left():
+
+def Move_Left(xpos, ypos):
     pass
 
 
 def Move_to_Position(xpos, ypos):
      if (x < xpos):
-         Move_Right()
+         Move_Right(xpos, ypos)
      else :
-         Move_Left()
+         Move_Left(xpos, ypos)
 
 while True:
     Move_to_Position(203, 535)
