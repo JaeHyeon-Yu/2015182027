4dbefcc169107d2124de13071355815271ab7386
JaeHyeon-Yu <43131666+JaeHyeon-Yu@users.noreply.github.com>
2018-09-20 16:47:16 +0900

    커서 움직임
    이제 마우스를 따라서 커서가 움직임
    Commit 02

--- Drill06/move_character_with_mouse.py
+++ Drill06/move_character_with_mouse.py
@@ -4,7 +4,17 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 
 def handle_events():
-    pass
+    global running
+    global x,y
+    events = get_events()
+
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_MOUSEMOTION:
+            x, y = event.x, KPU_HEIGHT -1 -event.y
+        elif event.type == SDL_KEYDOWN and event.key ==SDLK_ESCAPE:
+            running=False
 
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
