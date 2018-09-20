0747f8e4cfddcac2e5e3e8d6f24521f5e5f3804b
JaeHyeon-Yu <43131666+JaeHyeon-Yu@users.noreply.github.com>
2018-09-20 16:52:25 +0900

    캐릭터 그려짐
    이제 캐릭터도 그려짐. 이동 및 방향 수정해야함.
    Commit 03

--- Drill06/move_character_with_mouse.py
+++ Drill06/move_character_with_mouse.py
@@ -5,14 +5,14 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 def handle_events():
     global running
-    global x,y
+    global mouse_x, mouse_y
     events = get_events()
 
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, KPU_HEIGHT -1 -event.y
+            mouse_x, mouse_y = event.x, KPU_HEIGHT -1 -event.y
         elif event.type == SDL_KEYDOWN and event.key ==SDLK_ESCAPE:
             running=False
 
@@ -23,16 +23,18 @@ character = load_image('animation_sheet.png')
 mouse = load_image('hand_arrow.png')
 
 running = True
+mouse_x, mouse_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
-frame = 0
+frame_x = 0
 hide_cursor()
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    mouse.clip_draw(0, 0, 100, 100, x, y)
+    mouse.clip_draw(0, 0, 100, 100, mouse_x, mouse_y)
+    character.clip_draw(frame_x * 100, 100 * 1, 100, 100, x, 90)
     update_canvas()
-
+    frame_x = (frame_x + 1) % 8
     delay(0.02)
     handle_events()
 
