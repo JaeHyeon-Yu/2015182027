8cd1106319e0cc9342bc1a2c5ff577695dceb0e9
JaeHyeon-Yu <43131666+JaeHyeon-Yu@users.noreply.github.com>
2018-10-01 23:29:06 +0900

    Project : 전진카드 스프라이트 만듬
    Project 1주차
    전진카드의 스프라이트를 만들었다.

--- Drill05/character_moves.py
+++ Drill05/character_moves.py
@@ -1,12 +1,24 @@
 from pico2d import *
 
-open_canvas()
-
-grass = load_image('grass.png')
-character = load_image('animation_sheet.png')
+KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 x, y = 0+25, 90
 frame_x = 0
+
+def handle_events():
+    global running
+    global x, y
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_MOUSEMOTION:
+            x, y = event.x, KPU_HEIGHT - 1 - event.y
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+open_canvas(KPU_WIDTH, KPU_HEIGHT)
+grass = load_image('KPU_GROUND.png')
+character = load_image('animation_sheet.png')
 
 def Move_Animation(frame_y) :
     global x, y, frame_x
@@ -46,15 +58,15 @@ def Move_to_Position(xpos, ypos):
         Move_Left(xpos, ypos)
 
 while True:
-    Move_to_Position(203, 535)
-    Move_to_Position(132, 243)
-    Move_to_Position(535, 470)
-    Move_to_Position(477, 203)
-    Move_to_Position(715, 136)
-    Move_to_Position(316, 225)
-    Move_to_Position(510, 92)
-    Move_to_Position(692, 518)
-    Move_to_Position(682, 336)
-    Move_to_Position(712, 349)
+    #Move_to_Position(203, 535)
+   # Move_to_Position(132, 243)
+    #Move_to_Position(535, 470)
+    #Move_to_Position(477, 203)
+    #Move_to_Position(715, 136)
+    #Move_to_Position(316, 225)
+    #Move_to_Position(510, 92)
+    #Move_to_Position(692, 518)
+    #Move_to_Position(682, 336)
+    #Move_to_Position(712, 349)
 
 close_canvas()
