bc93ebe76569d428dcf0c4bfada9d19c1e2d891d
JaeHyeon-Yu <43131666+JaeHyeon-Yu@users.noreply.github.com>
2018-10-01 17:25:29 +0900

    Revert "Drill07 완성"
    This reverts commit bac7528aa9b192d807e15a145468423c0e43ac00.

--- Drill07/Drill07.py
+++ Drill07/Drill07.py
@@ -19,12 +19,6 @@ def Move_to_Position(p1, p2):
     global x, y
     global frame_x, frame_y
 
-    if p1[0] > p2[0]:
-        frame_y=0
-    else:
-        frame_y=100
-
-
     for i in range(0, 100+1, 2):
         clear_canvas()
         Ground.draw(1280 // 2, 1024 // 2)
@@ -34,16 +28,16 @@ def Move_to_Position(p1, p2):
         Character.clip_draw(frame_x * 100, frame_y, 100, 100, x, y)
         update_canvas()
         frame_x = (frame_x + 1) % 8
-        delay(0.01)
+        delay(0.1)
 
 while True:
     clear_canvas()
     Ground.draw(1280//2, 1024//2)
     update_canvas()
     Move_to_Position(points[n - 1], points[n])
-   # Character.clip_draw(frame_x * 100, frame_y, 100, 100, x, y)
+    Character.clip_draw(frame_x * 100, frame_y, 100, 100, x, y)
     update_canvas()
     frame_x = (frame_x + 1) % 8
-    delay(0.01)
+    delay(0.1)
     n = (n + 1) % size
 close_canvas()
