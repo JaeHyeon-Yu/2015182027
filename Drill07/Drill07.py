15204bbf8f41ec34b8fbff33bba7c0efd2993672
JaeHyeon-Yu <43131666+JaeHyeon-Yu@users.noreply.github.com>
2018-10-01 17:14:57 +0900

    이제 이동도 함
    이동은 하는데 순간이동함. 수정해야함.
    
    04

--- Drill07/Drill07.py
+++ Drill07/Drill07.py
@@ -11,17 +11,23 @@ n=1
 
 x, y=1280//2, 90
 frame_x = 0
-frame_x = 100
+frame_y = 100
 
 def Move_to_Position(p1, p2):
-    pass
+    global x, y
+    for i in range(0, 100+1, 2):
+        t= i/100
+        x=(1-t)*p1[0]+t*p2[0]
+        y=(1-t)*p1[1]+t*p2[1]
 
 while True:
     clear_canvas()
     Ground.draw(1280//2, 1024//2)
     update_canvas()
-    Character.clip_draw(frame_x * 100, 100, 100, 100, x, y)
+    Move_to_Position(points[n - 1], points[n])
+    Character.clip_draw(frame_x * 100, frame_y, 100, 100, x, y)
     update_canvas()
     frame_x = (frame_x + 1) % 8
-    delay(0.01)
+    delay(0.5)
+    n = (n + 1) % size
 close_canvas()
