cea4d2c5d633e0855533004376a875638ee7670c
JaeHyeon-Yu <43131666+JaeHyeon-Yu@users.noreply.github.com>
2018-10-01 17:19:15 +0900

    자연스럽게 이동함
    좌측을 보게끔 수정하자!

--- Drill07/Drill07.py
+++ Drill07/Drill07.py
@@ -6,19 +6,29 @@ Ground = load_image('KPU_GROUND.png')
 Character = load_image('animation_sheet.png')
 
 size=20
-points=[(random.randint(-500,500), random.randint(-350,350)) for i in range(size)]
+points=[(random.randint(0,1280), random.randint(0,1024)) for i in range(size)]
 n=1
 
 x, y=1280//2, 90
 frame_x = 0
 frame_y = 100
 
+
+
 def Move_to_Position(p1, p2):
     global x, y
+    global frame_x, frame_y
+
     for i in range(0, 100+1, 2):
+        clear_canvas()
+        Ground.draw(1280 // 2, 1024 // 2)
         t= i/100
         x=(1-t)*p1[0]+t*p2[0]
         y=(1-t)*p1[1]+t*p2[1]
+        Character.clip_draw(frame_x * 100, frame_y, 100, 100, x, y)
+        update_canvas()
+        frame_x = (frame_x + 1) % 8
+        delay(0.1)
 
 while True:
     clear_canvas()
@@ -28,6 +38,6 @@ while True:
     Character.clip_draw(frame_x * 100, frame_y, 100, 100, x, y)
     update_canvas()
     frame_x = (frame_x + 1) % 8
-    delay(0.5)
+    delay(0.1)
     n = (n + 1) % size
 close_canvas()
