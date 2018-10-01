fb7f52ca3cb6c40d9ce8360a8fbe382323cc9d87
JaeHyeon-Yu <43131666+JaeHyeon-Yu@users.noreply.github.com>
2018-10-01 17:02:24 +0900

    랜덤 리스트 구현
    좌표값을 랜덤으로 가지는 리스트를 구현했다!
    02

--- Drill07/Drill07.py
+++ Drill07/Drill07.py
@@ -5,7 +5,9 @@ open_canvas(1280, 1024)
 Ground = load_image('KPU_GROUND.png')
 Character = load_image('animation_sheet.png')
 
-
+size=20
+points=[(random.randint(-500,500), random.randint(-350,350)) for i in range(size)]
+n=1
 
 while True:
     clear_canvas()
