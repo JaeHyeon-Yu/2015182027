9e6a0e9763e6a3d25d63221f18ddc7ed91c01956
JaeHyeon-Yu <43131666+JaeHyeon-Yu@users.noreply.github.com>
2018-10-01 17:05:58 +0900

    캐릭터 띄움
    스크린에 캐릭터를 띄움
    이동 구현해야함
    03

--- Drill07/Drill07.py
+++ Drill07/Drill07.py
@@ -9,9 +9,19 @@ size=20
 points=[(random.randint(-500,500), random.randint(-350,350)) for i in range(size)]
 n=1
 
+x, y=1280//2, 90
+frame_x = 0
+frame_x = 100
+
+def Move_to_Position(p1, p2):
+    pass
+
 while True:
     clear_canvas()
     Ground.draw(1280//2, 1024//2)
     update_canvas()
-
+    Character.clip_draw(frame_x * 100, 100, 100, 100, x, y)
+    update_canvas()
+    frame_x = (frame_x + 1) % 8
+    delay(0.01)
 close_canvas()
