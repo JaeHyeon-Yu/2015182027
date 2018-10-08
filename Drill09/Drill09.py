c9ca88c95c0d7ddca6ce9480de623db0a09f5c41
JaeHyeon-Yu <zosua18@gmail.com>
2018-10-08 16:56:42 +0900

    Ball 클래스 채웠다!
    근데 그리는 좌표값 아마 수정해야할거임.
    이따가 그릴때 수정하자

--- Drill09/Drill09.py
+++ Drill09/Drill09.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import random
 
 class Grass:
     def __init__(self):
@@ -19,6 +20,18 @@ class Boy:
 
     def draw(self):
         self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
-        
+
 class Ball:
-    pass
+    def __init__(self):
+        self.x, self.y = random.randint(100, 700), random.randint(500, 600)
+        self.size=random.randint(1,2)
+        if self.size == 1:
+            self.image=load_image('ball21x21.png')
+        else:
+            self.image=load_image('ball41x24')
+
+    def update(self):
+        self.y -= 5
+
+    def draw(self):
+        self.image.clip_draw(0, 0, 100, 100, self.x, self.y)
