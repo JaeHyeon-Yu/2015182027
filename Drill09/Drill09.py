74dcbd81f297db3f9b8fdb524186262d76f5717d
JaeHyeon-Yu <zosua18@gmail.com>
2018-10-08 16:58:56 +0900

    Drill09 변수들 선언, 초기화함
    선언, 초기화하고 이벤트 함수도 넣었음!
    빨리빨리해서 오늘은 끝내자!

--- Drill09/Drill09.py
+++ Drill09/Drill09.py
@@ -35,3 +35,23 @@ class Ball:
 
     def draw(self):
         self.image.clip_draw(0, 0, 100, 100, self.x, self.y)
+
+
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+
+open_canvas()
+
+team=[Boy() for i in range(11)]
+grass = Grass()
+balls = [Ball() for i in range(20)]
+
+running = True
