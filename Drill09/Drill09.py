f2b864d6cfc34ef9b46dbcd6c47137034f8b959c
JaeHyeon-Yu <zosua18@gmail.com>
2018-10-08 16:50:10 +0900

    Grass 채웠다!
    Grass 채웠다! 이제 나머지도 채우자!
    02

--- Drill09/Drill09.py
+++ Drill09/Drill09.py
@@ -1,7 +1,12 @@
 from pico2d import *
 
 class Grass:
-    pass
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 30)
+        
 class Boy:
     pass
 class Ball:
