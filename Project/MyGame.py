7439e37a15501a9e7d91feab21389d25afef3694
JaeHyeon-Yu <43131666+JaeHyeon-Yu@users.noreply.github.com>
2018-10-10 22:53:30 +0900

    Project : Card 클래스 변수 선언!
    Project 2주차
    Card클래스의 변수를 선언했다.
    현재 카드수인 10개의 배열로 선언함!

--- Project/MyGame.py
+++ Project/MyGame.py
@@ -4,6 +4,7 @@ class Card:
     def __init__(self):
         self.selct = False
         self.useful = True
+        self.image
 class Player:
     def __init__(self):
         self.x, self.y
@@ -19,3 +20,5 @@ class Background:
         self.image1=load_image('map1.jpg')
         self.image2=load_image('map2.png')
         self.data[6][4]
+
+card=[Card() for i in range(10)]
