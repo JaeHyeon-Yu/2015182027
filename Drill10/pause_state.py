47b7aca2c3be5a97f1a2d855822f56dd4fed798b
JaeHyeon-Yu <43131666+JaeHyeon-Yu@users.noreply.github.com>
2018-10-11 21:10:46 +0900

    Drill10 정지화면 구현
    정지화면은 나오지만 실제 정지는안됨
    소년 위치가 리셋됨

--- Drill10/pause_state.py
+++ Drill10/pause_state.py
@@ -1,5 +1,6 @@
 import game_framework
 from pico2d import *
+import main_state
 
 name= "PauseState"
 image= None
@@ -11,3 +12,26 @@ def enter():
 def exit():
     global image
     del(image)
+
+def draw():
+    global image
+    clear_canvas()
+    image.draw(400, 300)
+    update_canvas()
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_KEYDOWN and event.key == SDLK_p:
+            game_framework.change_state(main_state)
+
+def update():
+    pass
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
