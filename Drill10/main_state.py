369a67c3b9a594733169b60f08ec1fa11b895429
JaeHyeon-Yu <43131666+JaeHyeon-Yu@users.noreply.github.com>
2018-10-11 21:54:58 +0900

    Drill10-1 Complete
    2015182027 유재현
    Commit : 06
    https://www.youtube.com/watch?v=m1VP1LEhnws&feature=youtu.be

--- Drill10/main_state.py
+++ Drill10/main_state.py
@@ -17,7 +17,6 @@ grass = None
 font = None
 
 
-
 class Grass:
     def __init__(self):
         self.image = load_image('grass.png')
@@ -32,15 +31,15 @@ class Boy:
         self.x, self.y = 0, 90
         self.frame = 0
         self.image = load_image('run_animation.png')
-        self.dir = 1
+        self.dir = 0.2
 
     def update(self):
         self.frame = (self.frame + 1) % 8
         self.x += self.dir
         if self.x >= 800:
-            self.dir = -1
+            self.dir = -0.2
         elif self.x <= 0:
-            self.dir = 1
+            self.dir = 0.2
 
     def draw(self):
         self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
@@ -74,7 +73,7 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key==SDLK_ESCAPE:
             game_framework.change_state(title_state)
         elif event.type == SDL_KEYDOWN and event.key == SDLK_p:
-            game_framework.change_state(pause_state)
+            game_framework.push_state(pause_state)
 
 
 def update():
--- Drill10/pause_state.py
+++ Drill10/pause_state.py
@@ -4,6 +4,7 @@ import main_state
 
 name= "PauseState"
 image= None
+pause_dir = True
 
 def enter():
     global image
@@ -23,7 +24,8 @@ def handle_events():
     events = get_events()
     for event in events:
         if event.type == SDL_KEYDOWN and event.key == SDLK_p:
-            game_framework.change_state(main_state)
+            game_framework.pop_state()
+
 
 def update():
     pass
