d9b3236d915fe75c10cb5f652849ce9c75150024
JaeHyeon-Yu <43131666+JaeHyeon-Yu@users.noreply.github.com>
2018-10-12 21:34:18 +0900

    Project : main_State의 뼈대잡음
    Project 2주차
    main_state의 뼈대를 만들었다.
    함수들의 내용은 pass로 채움.

---
+++ Project/main_state.py
@@ -0,0 +1,26 @@
+from pico2d import *
+import game_framework
+import title_state
+
+name= 'MainState'
+
+def enter():
+    pass
+
+def exit():
+    pass
+
+def handle_events():
+    pass
+
+def draw():
+    pass
+
+def update():
+    pass
+
+def pause():
+    pass
+
+def resume():
+    pass
