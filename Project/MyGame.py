7f1f2e77babe2ddcfd7d7a9da9e442e8e8505ae0
JaeHyeon-Yu <43131666+JaeHyeon-Yu@users.noreply.github.com>
2018-10-12 21:15:27 +0900

    Project : Monster 클래스 수정
    Project 2주차
    Monster 클래스에 hp, mp, 대미지 정보를 추가했다.

--- Project/MyGame.py
+++ Project/MyGame.py
@@ -14,6 +14,8 @@ class Monster:
     def __init__(self):
         self.x, self.y
         self.frame_x, self.frame_y
+        self.hp, self.mp
+        self.damage
 
 class Background:
     def __init__(self):
@@ -22,3 +24,4 @@ class Background:
         self.data[6][4]
 
 card=[Card() for i in range(10)]
+
