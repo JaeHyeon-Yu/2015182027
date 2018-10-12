67145c6ef8bcd315c484df7167c34a5247f07cfc
JaeHyeon-Yu <43131666+JaeHyeon-Yu@users.noreply.github.com>
2018-10-12 21:41:57 +0900

    Project : 메인파일 분할
    Project 2주차
    메인파일에서 클래스들을 새로운 파일로 분할함.

--- Project/MyGame.py
+++ Project/MyGame.py
@@ -1,29 +1,6 @@
 from pico2d import *
 import game_framework
 import title_state
-
-class Card:
-    def __init__(self):
-        self.selct = False
-        self.useful = True
-        self.image
-class Player:
-    def __init__(self):
-        self.x, self.y
-        self.frame_x, self.frame_y
-        self.image
-class Monster:
-    def __init__(self):
-        self.x, self.y
-        self.frame_x, self.frame_y
-        self.hp, self.mp
-        self.damage
-
-class Background:
-    def __init__(self):
-        self.image1
-        self.image2
-        self.data[6][4]
-
+import main_state
 
 open_canvas()
--- Project/main_state.py
+++ Project/main_state.py
@@ -1,6 +1,7 @@
 from pico2d import *
 import game_framework
 import title_state
+import game_class
 
 name= 'MainState'
 
