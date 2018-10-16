5f5d636335fa3cc0ab2ffcf3e3d4a93ce480ff15
JaeHyeon-Yu <43131666+JaeHyeon-Yu@users.noreply.github.com>
2018-10-16 18:00:16 +0900

    Project : title_state 채움
    Project 3주차
    title_state의 내용을 채웠다. 내용물은 모두 pass로

--- Project/title_state.py
+++ Project/title_state.py
@@ -1,3 +1,28 @@
 from pico2d import *
 import game_framework
 import main_state
+
+
+name= "TitleState"
+image= None
+
+def enter():
+    pass
+
+def exit():
+    pass
+
+def handle_events():
+    pass
+
+def draw():
+    pass
+
+def update():
+    pass
+
+def pause():
+    pass
+
+def resume():
+    pass
