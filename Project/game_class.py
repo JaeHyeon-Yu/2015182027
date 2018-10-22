c2cb9dbb9ab2f803324821b88b9d5d2021184202
JaeHyeon-Yu <43131666+JaeHyeon-Yu@users.noreply.github.com>
2018-10-23 03:43:47 +0900

    Project : 플레이어 스프라이트 띄움
    Project 4주차
    플레이어 스프라이트를 띄우기까지만! 했다.

--- Project/game_class.py
+++ Project/game_class.py
@@ -4,11 +4,19 @@ from card_class import *
 
 class Player:
     def __init__(self):
-        self.x, self.y
-        self.frame_x, self.frame_y
+        self.x, self.y = None, None
+        self.frame_x, self.frame_y = None, None
         self.image = None
     def Initialize(self):
-        pass
+        self.x, self.y = 100,  400
+        self.image = load_image('sprites/zeroxsheet.gif')
+        self.frame_x, self.frame_y = 0, 350
+    def draw(self):
+        self.image.clip_draw((self.frame_x*54), self.frame_y, 54, 50, self.x, self.y)
+    def update(self):
+        self.frame_x = (self.frame_x+1) % 16
+
+
 class Monster:
     def __init__(self):
         self.x, self.y
@@ -19,4 +27,8 @@ class Monster:
 class Background:
     def __init__(self):
         self.image = None
-        self.data[4][3]
+    def Initialize(self, image):
+        self.image = image
+
+    def draw(self):
+        self.image.draw(400, 300)
--- Project/main_state.py
+++ Project/main_state.py
@@ -1,18 +1,22 @@
 from pico2d import *
 import game_framework
-import start_state
+import title_state
 import game_class
 
 name= 'MainState'
-#hero = game_class.Player()
+hero = game_class.Player()
+map = game_class.Background()
 image = None
 
 def enter():
     global image
-    image = load_image('sprites/map1.jpg')
+    image = load_image('sprites/stage.png')
+    hero.Initialize()
+    map.Initialize(load_image('sprites/map1.jpg'))
 
 def exit():
-    pass
+    global image
+    del(image)
 
 def handle_events():
     events = get_events()
@@ -22,11 +26,17 @@ def handle_events():
 
 def draw():
     clear_canvas()
+    map.draw()
     image.draw(400, 300)
+    hero.draw()
+
+    for card in title_state.card_stack:
+        card.draw()
     update_canvas()
+    delay(0.1)
 
 def update():
-    pass
+    hero.update()
 
 def pause():
     pass
