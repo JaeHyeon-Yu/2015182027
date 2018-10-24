37d3d6002170fe1f7e69f6930552d4f19727cd1e
JaeHyeon-Yu <43131666+JaeHyeon-Yu@users.noreply.github.com>
2018-10-25 01:36:10 +0900

    Project: 점프 구현
    Project 4주차
    점프를 구현하였다.

--- Project/player_class.py
+++ Project/player_class.py
@@ -9,6 +9,7 @@ class Player:
         self.frame_x, self.frame_y = None, None
         self.size_x, self.size_y = 80, 100
         self.image = None
+        self.before = None
 
         self.gun = False
         self.bullet = Bullet()
@@ -16,7 +17,7 @@ class Player:
         self.animation = 0
         self.Idle_animation = [(0, 900), (75, 900)]   # 0
         self.run_animation = [(0, 500), (80, 500), (160, 500), (240, 500), (320, 500), (400, 500), (480, 500), (570, 500), (650, 500), (740, 500), (820, 500), (895, 500), (975, 500), (1055, 500), (1135, 500), (1220, 500)]    # 1
-        self.jump_animation = [(0, 760), (70, 745), (137, 740)]  # 3    # 나중에 찾자.....
+        self.jump_animation = [(0, 760), (70, 745), (202, 740), (270, 740), (202, 740), (70, 745)]  # 3    # 나중에 찾자.....
         self.gun_animation = [(0, 630), (75, 630), (155, 630), (240, 630), (325, 630), (408, 630), (492, 630), (600, 630), (325, 630), (240, 630), (155, 630)]  # 9
         self.attack_animation = [(0, 420), (90, 420), (182, 420), (265, 420), (440, 420), (605, 420), (760, 420), (880, 420), (1000, 420), (90, 420)]  # 5
         self.frame = 0
@@ -37,27 +38,32 @@ class Player:
         elif self.animation is 1:
             self.Run_Animation()
 
+        elif self.animation is 2:
+            self.Back_Animation()
+
         elif self.animation is 3:
-            pass            # jump
+            self.Jump_Animation()
 
         elif self.animation is 4:
-            pass            # Down
+            self.Down_Animation()
 
         elif self.animation is 5:
             self.Attck_Animation()
 
         elif self.animation is 6:
-            pass
+            self.Defense_Animation()
 
         elif self.animation is 7:
-            pass
+            self.Heal_Animation()
 
         elif self.animation is 8:
-            pass
+            self.Mana_Animation()
 
-        elif self.animation is 9:  # gun
+        elif self.animation is 9:
             self.Gun_Animation()
 
+        elif self.animation is 10:
+            self.Fire_Animation()
 
         if self.gun is True:
             self.bullet.update()
@@ -80,9 +86,20 @@ class Player:
             self.x = 300
             self.frame = 0
             self.animation = 0
-    # 2
-    # 3
-    # 4
+    def Back_Animation(self):
+        pass #2
+
+    def Jump_Animation(self):
+        self.frame_x, self.frame_y = self.jump_animation[self.frame]
+        self.frame = (self.frame + 1) % 6
+        self.y += 200//6
+
+        if self.frame is 5:
+            self.frame = 0
+            self.animation = 0
+
+    def Down_Animation(self):
+        pass #4
     def Attck_Animation(self):
         if self.frame is 3 or self.frame is 4:
             self.size_x = 160
@@ -112,8 +129,12 @@ class Player:
         if self.frame is 9:
             self.frame = 0
             self.animation = 0
-    # 7
-    # 8
+    def Defense_Animation(self):
+        pass #6
+    def Heal_Animation(self):
+        pass #7
+    def Mana_Animation(self):
+        pass #8
     def Gun_Animation(self):
         if self.frame is 5:
             self.size_x = 88
@@ -132,4 +153,5 @@ class Player:
         if self.frame is 10:
             self.frame = 0
             self.animation = 0
-    # 10
+    def Fire_Animation(self):
+        pass #10
